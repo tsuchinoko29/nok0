3000c060a3567a08e99684da0924e67065d15068
tsuchinoko29 <yanyanyanwan@gmail.com>
2021-09-08 09:57:46 +0900

    add hld

---
+++ test/yosupo-vertex_add_path_sum.test.cpp
@@ -0,0 +1,26 @@
+#include "data_structure/binary_indexed_tree.hpp"
+#include "graph/graph.hpp"
+#include "graph/hld.hpp"
+#include "template.hpp"
+
+void main_() {
+	INT(n, q);
+	VEC(int, a, n);
+	Graph g(n, -1, 0, 0, 0);
+
+	heavy_light_decomposition hld(g);
+
+	binary_indexed_tree<ll> bt(n);
+	REP(i, n) { bt.add(hld.in[i], a[i]); }
+
+	auto f = [](ll a, ll b) { return a + b; };
+	auto query = [&](int l, int r) { return bt.sum(l, r); };
+
+	while(q--) {
+		INT(type, u, v);
+		if(type == 0)
+			bt.add(hld.in[u], v);
+		if(type == 1)
+			print(hld.query(u, v, 0ll, query, f));
+	}
+}
---
+++ test/yosupo-vertex_add_subtree_sum.test.cpp
@@ -0,0 +1,34 @@
+#include "data_structure/binary_indexed_tree.hpp"
+#include "graph/graph.hpp"
+#include "graph/hld.hpp"
+#include "template.hpp"
+
+void main_() {
+	INT(n, q);
+	VEC(int, a, n);
+	Graph g(n);
+	REP(i, 1, n) {
+		INT(p);
+		g.add_edge(i, p);
+	}
+
+	heavy_light_decomposition hld(g);
+
+	binary_indexed_tree<ll> bt(n);
+	REP(i, n) { bt.add(hld.in[i], a[i]); }
+
+	auto f = [](ll a, ll b) { return a + b; };
+	auto query = [&](int l, int r) { return bt.sum(l, r); };
+
+	while(q--) {
+		INT(type, u);
+		if(type == 0) {
+			INT(v);
+			bt.add(hld.in[u], v);
+		}
+		if(type == 1) {
+			auto [l, r] = hld.subtree(u);
+			print(bt.sum(l, r));
+		}
+	}
+}
