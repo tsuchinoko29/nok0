b30d42f8576a6eb1af2d8e827765d7ef162bff02
tsuchinoko29 <yanyanyanwan@gmail.com>
2021-09-08 15:43:05 +0900

    add fps verify

---
+++ test/yosupo-log_of_formal_power_series.test.cpp
@@ -0,0 +1,14 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/log_of_formal_power_series"
+
+#include "atcoder/modint"
+#include "math/formal_power_series.hpp"
+#include "template.hpp"
+
+using mint = atcoder::modint998244353;
+using fps = formal_power_series<mint, FAST>;
+void main_() {
+	INT(n);
+	fps f(n);
+	cin >> f;
+	print(f.log());
+}
